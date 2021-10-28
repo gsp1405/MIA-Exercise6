9d428d8cc8c86df6e9b024cc38c0873d3d80b187
Czelucha <90610175+Czelucha@users.noreply.github.com>
2021-10-28 15:36:04 +0200

    compute miu, sig2, pi

--- computeParameters.m
+++ computeParameters.m
@@ -1,10 +1,10 @@
-function [miu, sigma, pi] = computeParameters(d, w)
+function [miu, sig2, pi] = computeParameters(d, w)
+% function computes values of mean (miu), variance (sig2) and probabilities (pi)
 % d - intensity image
 % w - posterior
 % N - number of voxel
-% sigma is a sigma square
 N = size(d);
 pi = sum(w)/N;
 miu = sum(w*d)/sum(w);
-sigma = sum(w*(d-miu*ones(size(d,1), size(d,2)))^2)/sum(w);
+sig2 = sum(w*(d-miu*ones(size(d,1), size(d,2)))^2)/sum(w);
 end
